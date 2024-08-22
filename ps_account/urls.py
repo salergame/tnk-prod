115d4e5523923146d94e3082cc4cb6eab4dc1461
Sanzhar <salergame07@gmail.com>
2024-08-23 00:57:30 +0500

    make base for site

---
+++ ps_account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from ps_account import views
+
+app_name='ps_account'
+
+urlpatterns = [
+    path('',views.account,name='account'),
+]
---
+++ ps_account/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def account(request):
+    return render(request, 'ps_account/sit2.html')
