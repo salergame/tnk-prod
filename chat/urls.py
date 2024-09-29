6466cc0eda9e4f2035de84581a9cda3263146865
Sanzhar <salergame07@gmail.com>
2024-09-30 01:51:17 +0500

    many chats created

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import *
+from .views import chat_views
 
 app_name = 'chat'
 
 urlpatterns = [
-    path('', chat_views, name='index'),
+    path('', chat_views, name='index'),  # Основная страница чатов
+    path('chatroom/<str:chatroom_name>/', chat_views, name='chatroom'),  # Путь для отдельных чатов
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,25 +1,39 @@
 from django.shortcuts import render, get_object_or_404, redirect 
 from django.contrib.auth.decorators import login_required
-from .models import *
+from .models import ChatGroup, GroupMessage
 from .forms import ChatmessageCreateForm
 
 @login_required
-def chat_views(request):
-    chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
-    chat_messages = chat_group.chat_messages.all()[:30]
-    form = ChatmessageCreateForm()
+def chat_views(request, chatroom_name="public-chat"):
+    # Получаем текущую группу чата
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
     
+    # Загружаем последние 30 сообщений из текущего чата
+    chat_messages = chat_group.chat_messages.all()[:30]
+    
+    # Загружаем список всех чатов для staff
+    if request.user.is_staff:
+        chat_list = ChatGroup.objects.filter(users_online=request.user)  # Убедись, что фильтр правильный
+    else:
+        chat_list = []
+
+    # Проверка на запрос через htmx
     if request.htmx:
         form = ChatmessageCreateForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             message = form.save(commit=False)
             message.author = request.user
             message.group = chat_group
             message.save()
             context = {
-                'message' : message,
-                'user' : request.user
+                'message': message,
+                'user': request.user
             }
             return render(request, 'chat/partials/chat_message_p.html', context)
-    
-    return render(request, 'chat/chat.html', {'chat_messages' : chat_messages, 'form' : form})
+
+    return render(request, 'chat/chat.html', {
+        'chat_messages': chat_messages,  # Сообщения из текущего чата
+        'form': ChatmessageCreateForm(),
+        'chat_list': chat_list,  # Список чатов для staff
+        'current_chatroom': chat_group  # Текущая комната
+    })
