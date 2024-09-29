a5d2e238975b5ce8fcfd26d84a54c229564a95ce
Sanzhar <salergame07@gmail.com>
2024-09-30 00:40:52 +0500

    track online

--- chat/consumers.py
+++ chat/consumers.py
@@ -16,6 +16,10 @@ class ChatroomConsumer(WebsocketConsumer):
             self.channel_name
         )
         
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+        
         self.accept()
         
     def disconnect(self, close_code):
@@ -23,6 +27,10 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name,
             self.channel_name
         )
+        
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
         
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -54,3 +62,17 @@ class ChatroomConsumer(WebsocketConsumer):
         
         self.send(text_data=html)
     
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count() -1
+        event = {
+            'type':'online_count_handler',
+            'online_count':online_count
+        }
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name, event
+        )
+    
+    def online_count_handler(self, event):
+        online_count = event['online_count']
+        html = render_to_string("chat/partials/online_count.html",{'online_count':online_count})
+        self.send(text_data=html)
