fadc4b64ae1d783a6c9a63c8d34a966822c85f87
Sanzhar <salergame07@gmail.com>
2025-01-07 18:26:20 +0500

    fix

--- tnk_ocenka/settings.py
+++ tnk_ocenka/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config('tnk-prod.onrender.com')
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1", cast=Csv())
 
 CSRF_TRUSTED_ORIGINS = ['https://tnk-prod.onrender.com']  # Добавьте свои домены
 
