48f6d7cda40c3f860c3ffb267a179ded2c0b4b9b
Sanzhar <salergame07@gmail.com>
2025-05-22 01:16:15 +0500

    money more more

--- tnk_ocenka/settings.py
+++ tnk_ocenka/settings.py
@@ -223,8 +223,8 @@ CORS_ALLOWED_ORIGINS = [
 SITE_ID = 1
 SITE_URL = 'https://tnk-ocenka.kz'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
 
 LOGGING = {
     'version': 1,
